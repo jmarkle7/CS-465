14786d67aa4ddfeac8a806a825d4b9a80ce04e3b
Jaydon Markle <jaydon@Jaydons-MacBook-Air.local>
2026-03-21 22:43:10 -0500

    Module 3 completed

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    const tripsPath = path.join(__dirname, '../../data/trips.json');
+    const trips = JSON.parse(fs.readFileSync(tripsPath, 'utf8'));
+    res.render('travel', {
+        title: 'Travlr Getaways',
+        trips
+    });
 };
 
 module.exports = {
